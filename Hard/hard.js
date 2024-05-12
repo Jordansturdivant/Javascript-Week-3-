76df31b79a5e368a86035d1d60939f66e5332f8e
Jordansturdivant <165715211+Jordansturdivant@users.noreply.github.com>
2024-05-12 12:16:35 -0400

    initial commit

---
+++ Hard/hard.js
@@ -0,0 +1,30 @@
+function createPatient(name, ssn) {
+    // Private object
+    var PII = {
+      name: name,
+      SSN: ssn
+    };
+  
+    // Public function to access name property
+    function getName() {
+      return PII.name;
+    }
+  
+    // Public function to access SSN property
+    function getSSN() {
+      return PII.SSN;
+    }
+  
+    // Return an object with public methods only
+    return {
+      getName: getName,
+      getSSN: getSSN
+    };
+  }
+  
+  var patient2 = createPatient("John", "123-45-6789");
+  
+  console.log(patient2.names); 
+  console.log(patient2.ssn); 
+  console.log(patient2.getName()); 
+  console.log(patient2.getSSN())
